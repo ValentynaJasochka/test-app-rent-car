a1c713a9bb673adad5a7b69136295ff1f33b7b4c
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2024-01-20 23:30:54 +0200

    update-index

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const theme = {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-  
+  <React.StrictMode>
     <ThemeProvider theme={theme}>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
@@ -30,5 +30,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </PersistGate>
       </Provider>
     </ThemeProvider>
+    </React.StrictMode>
   </>
 );
