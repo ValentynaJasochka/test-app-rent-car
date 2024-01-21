89756ff40b795db0cee4a96ebc76de2485c2c93f
ValentynaJasochka <125204092+ValentynaJasochka@users.noreply.github.com>
2024-01-21 20:14:35 +0200

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,8 @@
 
 // import { Loader } from 'components/Loader/Loader';
 
+import { Banner } from "./Home.styled";
+
 
 
 const Home = () => {
@@ -8,7 +10,8 @@ const Home = () => {
 
   return (
     <div>
-      <h1>HOME</h1>
+      <h1>New Car Rental in Ukraine with Address Delivery within an Hour</h1>
+      <Banner></Banner>
       
       {/* {loading && <Loader />} */}
      
--- src/pages/Home/Home.styled.jsx
+++ src/pages/Home/Home.styled.jsx
@@ -1,4 +1,14 @@
 import styled from 'styled-components';
+import { ReactComponent as BannerImg } from '../../image/banner.webp';
 export const Title = styled.h1`
+font-weight: 600;
+font-size: 22px;
   color: navy;
+`;
+export const Banner = styled.div`
+width: 100%;
+height: 700px;
+  background-image: url(https://static9.depositphotos.com/1001545/1133/i/450/depositphotos_11337456-stock-photo-happy-girl-in-a-car.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
